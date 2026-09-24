b9246ff2a68b04067b49ef0d22238cbe73823cba
Juan Henao <solosoyjuan03@gmail.com>
2026-09-23 21:40:11 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -3,8 +3,28 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def inicio():
+def home():
     return render_template('index.html')
+
+@app.route('/comunidad')
+def comunidad():
+    return render_template('Comunidad/comunidad.html')
+
+@app.route('/proyectos')
+def proyectos():
+    return render_template('Proyectos/proyectos.html')
+
+@app.route('/consultoria')
+def consultoria():
+    return render_template('Consultoria/consultoria.html')
+
+@app.route('/tienda')
+def tienda():
+    return render_template('Tienda/tienda.html')
+
+@app.route('/blog')
+def blog():
+    return render_template('Blog/blog.html')
 
 if __name__=='__main__':
     app.run(debug=True)
